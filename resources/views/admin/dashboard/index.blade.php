858dfae705d9dee8844267cac5af5e1b2f9b3099
rizface <alfarizzi2711@gmail.com>
2023-12-18 22:19:22 +0700

    fix path for logo and profile picture

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -35,8 +35,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
-      <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="#" class="brand-link">
+      <img src="{{asset('/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -45,7 +45,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -54,20 +54,22 @@
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
+      {{-- Admin Course Menu --}}
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
                 <a href="{{route("admin.dashboard")}}" class="nav-link">
                   <i class="fas fa-circle nav-icon"></i>
-                  <p>Contest</p>
+                  <p>Course</p>
                 </a>
               </li>
             <li class="nav-item">
               <a href="{{route("admin.createContestPage")}}" class="nav-link">
                 <i class="fas fa-circle nav-icon"></i>
-                <p>Create Contest</p>
+                <p>Create Course</p>
               </a>
             </li>
         </ul>
+      {{-- Admin Course Menu --}}
       </nav>
       <!-- /.sidebar-menu -->
     </div>
