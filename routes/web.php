77c256536e33593f10301d174ca2e2d1ec77b005
rizface <alfarizzi2711@gmail.com>
2023-12-18 22:19:05 +0700

    change path from contest to course

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,12 @@ Route::prefix("/admin")->group(function() {
     Route::post("/login", [AdminController::class, "Login"])->name("admin.login");
 
     Route::get('/', [ContestController::class, "Dashboard"])->name("admin.dashboard");
-    Route::get("/contest", [ContestController::class, "CreateContestPage"])->name("admin.createContestPage");
-    Route::post("/contest", [ContestController::class, "CreateContest"])->name("admin.createContest");
-    Route::get('/contest/{id}', [ContestController::class, "DetailContestPage"])->name("admin.contestDetailPage");
-    Route::get('/contest/{id}/question', [ContestController::class, "CreateQuestionPage"])->name("admin.createQuestionPage");
-    Route::post("/contest/{id}/question", [ContestController::class, "CreateQuestion"])->name("admin.createQuestion");
-    Route::get('/contest/{id}/question/{questionId}', [ContestController::class, "DetailQuestionPage"])->name("admin.questionDetailPage");
+    Route::get("/course", [ContestController::class, "CreateContestPage"])->name("admin.createContestPage");
+    Route::post("/course", [ContestController::class, "CreateContest"])->name("admin.createContest");
+    Route::get('/course/{id}', [ContestController::class, "DetailContestPage"])->name("admin.contestDetailPage");
+    Route::get('/course/{id}/question', [ContestController::class, "CreateQuestionPage"])->name("admin.createQuestionPage");
+    Route::post("/course/{id}/question", [ContestController::class, "CreateQuestion"])->name("admin.createQuestion");
+    Route::get('/course/{id}/question/{questionId}', [ContestController::class, "DetailQuestionPage"])->name("admin.questionDetailPage");
 });
 
 Route::get('/', function () {
