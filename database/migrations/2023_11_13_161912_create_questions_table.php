86e1650c869b52898d1e573a4b9cdc91ad539a14
rizface <alfarizzi2711@gmail.com>
2023-12-20 21:20:30 +0700

    add onDelete constraint for contest_id in questions table

--- database/migrations/2023_11_13_161912_create_questions_table.php
+++ database/migrations/2023_11_13_161912_create_questions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->uuid("id")->primary();
-            $table->foreignUuid("contest_id")->references("id")->on("contests");
+            $table->foreignUuid("contest_id")->references("id")->on("contests")->onDelete("cascade");
             $table->string("title");
             $table->text("description");
             $table->jsonb("test_cases");
