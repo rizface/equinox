f60be5d3216c4731a88cde76c3521bec3237049a
rizface <alfarizzi2711@gmail.com>
2024-03-24 10:50:12 +0700

    fix broken link

--- resources/views/coder/dashboard/profile.blade.php
+++ resources/views/coder/dashboard/profile.blade.php
@@ -98,7 +98,7 @@
                     <!-- Post -->
                         <div class="post">
                             <div class="user-block">
-                            <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
+                            <img class="img-circle img-bordered-sm" src="{{asset('dist/img/user1-128x128.jpg')}}" alt="user image">
                             <span class="username">
                                 <a href="#" class="text-capitalize">{{$coder->name}}.</a>
                                 <a href="#" class="float-right btn-tool"><i class="fas fa-times"></i></a>
